6d3c21596b0e0b33208d6ac63d5c5421450bf6af
ayousefpour <yousefpour@live.com>
2022-05-26 12:55:36 -0700

    Update import calendar.py

--- import calendar.py
+++ import calendar.py
@@ -5,3 +5,7 @@ year = int(input("Please Enter the year Number: "))
 month = int(input("Please Enter the month Number: "))
 
 print(calendar.month(year, month))
+print(calendar.month(year, month))
+
+
+
